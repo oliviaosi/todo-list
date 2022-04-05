43ab7aff72d774059a43e37bcfd2700cbf474696
olivia zhao <68890723+oliviaosi@users.noreply.github.com>
2022-04-05 20:31:20 +0800

    todo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import TodoItem from "./components/TodoItem";
+
 
 function App() {
+  //STATE
+  const [todos, setTodos] = useState([
+    "Learn React",
+    "Learn react again",
+  ])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My to do list</h1>
+      {todos.map((todo,index) => (
+      <TodoItem todo={todo} key={index}/>
+      ))}
+      
     </div>
   );
 }
---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TodoItem(props){
+    const {todo} = props;
+    
+    return <div>{todo}</div>;
+}
+
+export default TodoItem;
+
+
