47b63f0661436bbd0949cbeb96d5e5a9acc8bf95
olivia zhao <68890723+oliviaosi@users.noreply.github.com>
2022-04-09 12:40:18 +0800

    yes

--- src/App.js
+++ src/App.js
@@ -1,54 +1,56 @@
 import { useState } from "react";
+
+// Components
 import TodoItem from "./components/TodoItem/TodoItem";
 import TodoForm from "./components/TodoForm/TodoForm";
 
-//css
+// CSS
 import "./App.css";
 
 function App() {
-  //STATE
+  // State
   const [todos, setTodos] = useState([
-    {text: "lEARN ABOUT React", isCompleted: false},
-    {text: "Meet friends for lunch", isCompleted: false},
-    {text: "Build cool React app", isCompleted: false},
+    { text: "Learn about React", isCompleted: false },
+    { text: "Meet friends for lunch", isCompleted: false },
+    { text: "Build really cool todo app in React", isCompleted: false },
   ]);
-  //Actions
 
-const addTodo = (text)=> {
-  const newTodos= [...todos, {text: text, isCompleted:false}];
-    // What it would it would like if you did not add the three dots before todos
-    // [["something", "something else"], "some todo"]
-    // With the three dots also called the spread operator 
-    // ["something", "something else", "some todo"]
-  setTodos(newTodos);
-}
+  // Actions
+  const addTodo = (text) => {
+    const newTodos = [...todos, { text: text, isCompleted: false }];
+    setTodos(newTodos);
+  };
 
-const completeTodo =(index) =>{
-  const newTodos =[...todos]
-  newTodos[index].isCompleted = true;
-  setTodos(newTodos);
-}
+  const completeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
 
-const removeTodo =  (index)=>{
-  const newTodos= [...todos];
-  newTodos.splice(index,1);
-  setTodos(newTodos);
-}
+  const removeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
   return (
-      <div className="app">
+    <div className="app">
       <div className="todo-list">
-      <h1>My to do list</h1>
-
-      {todos.map((todo,index) => (
-        <TodoItem todo={todo} key={index} indext={index}
-      completTodo={completeTodo}
-      removeTodo={removeTodo} />
-      ))}
-        <TodoForm addTodo={addTodo}/>
-      
-    </div>
+        <h1>My todo list</h1>
+        {todos.map((todo, index) => (
+          <TodoItem
+            todo={todo}
+            key={index}
+            index={index}
+            completeTodo={completeTodo}
+            removeTodo={removeTodo}
+          />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
     </div>
   );
 }
 
 export default App;
+
--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,23 +1,21 @@
 import React from "react";
 
+// CSS
+import "./TodoItem.css";
 
-//CSS
-import"./TodoItem.css";
+function TodoItem(props) {
+  const { todo, index, completeTodo, removeTodo } = props;
 
-function TodoItem(props){
-    const {todo, index, completeTodo, removeTodo} = props;
-
-    return (
-        <div className={`todo${todo.isCompleted ? "complete": ""}`}>
-        {todo.text}
-        <div>
+  return (
+    <div className={`todo ${todo.isCompleted ? "complete" : ""}`}>
+      {todo.text}
+      <div>
         <button onClick={() => completeTodo(index)}>Complete</button>
         <button onClick={() => removeTodo(index)}>X</button>
-        </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default TodoItem;
-
 
